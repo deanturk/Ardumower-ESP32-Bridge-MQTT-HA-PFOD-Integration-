752cb32c5e18419136c7a1301a9f544c61526fdd
deanturk <deant2903@gmail.com>
2026-05-02 22:12:06 +0200

    Update CommBridge.cpp

--- CommBridge.cpp
+++ CommBridge.cpp
@@ -1,16 +1,14 @@
 #include "CommBridge.h"
 #include "config.h"
-#include "mqttHandler.h"  // Da pozna funkcijo esp32_Mqtt_sta
+#include "mqttHandler.h"  
 #include <WiFi.h>
 
-// Povemo CommBridge-u, da ti objekti živijo v .ino datoteki
 extern WiFiServer TheServeur;
 extern WiFiClient pfodClient;
 extern HardwareSerial Serial2;
-// Če uporabljaš Bluetooth, odkomentiraj spodnjo vrstico
-extern BluetoothSerial SerialBT;
 
-// Ker si imel napako pri definiciji WIFIbuf, ga definiraj tukaj:
+extern BluetoothSerial SerialBT;     // uncomment if You use Bluetooth
+
 uint8_t WIFIbuf[my_bufferSize];
 uint16_t inWiFI = 0;
 extern char line_receive[256];
@@ -20,20 +18,19 @@ void handleCommBridge() {
   bool staReceived = false;
   static char mqttProcessingBuffer[256]; // Statičen buffer za varno obdelavo niza
 
-  // 1. Upravljanje WiFi PFOD odjemalcev (Standardno)
+  // 1. Manage WiFi PFOD clients (Standard)
   if (TheServeur.hasClient()) {
     if (!pfodClient || !pfodClient.connected()) {
       if (pfodClient) pfodClient.stop();
       pfodClient = TheServeur.available();
       if (debug) Serial.println("New WIFI client connected");
     } else {
-      // Zavrni dodatne kliente, če je eden že povezan
       WiFiClient tempClient = TheServeur.available();
       tempClient.stop();
     }
   }
 
-  // 2. Podatki iz PFOD (WiFi) -> DUE (Serial2)
+  // 2. Data from PFOD (WiFi) -> DUE (Serial2)
   if (pfodClient && pfodClient.available()) {
     int availableWiFi = pfodClient.available();
     if (availableWiFi > my_bufferSize) availableWiFi = my_bufferSize;
@@ -44,29 +41,29 @@ void handleCommBridge() {
     }
   }
 
-  // 3. Podatki iz DUE (Serial2) -> WiFi/BT in MQTT analiza
+  // 3. Data from DUE (Serial2) -> WiFi/BT in MQTT analize
+  //    DUE sends some 10 data to console starts with #RMSTA: STATE, STATUS, BATVOLT etc 
   if (Serial2.available()) {
-    inWiFI = 0; // Resetiramo števec za paket, ki gre na WiFi
-
+    inWiFI = 0; // Reset counter for packet 
+    
     while (Serial2.available()) {
       char aChar = Serial2.read();
 
-      // Polnjenje bufferja za takojšnje pošiljanje na WiFi/BT (pfodApp)
+      // Fill bufferj to send on WiFi/BT (pfodApp)
       if (inWiFI < my_bufferSize - 1) {
         WIFIbuf[inWiFI++] = aChar;
       }
 
-      // Polnjenje line_receive bufferja za MQTT analizo
+      // Fill line_receive buffer for MQTT analize
       if (aChar == '\n') {
-        line_receive[mon_index] = '\0'; // Zaključi niz
+        line_receive[mon_index] = '\0';
         
-        // Če najdemo glavo statusa, si jo prekopiramo za kasnejšo obdelavo
         if (strncmp(line_receive, "#RMSTA", 6) == 0) {
           strncpy(mqttProcessingBuffer, line_receive, sizeof(mqttProcessingBuffer));
           staReceived = true; 
         }
         
-        mon_index = 0; // Priprava na naslednjo vrstico
+        mon_index = 0; // Prepare for next line data
       } else {
         if (mon_index < 255) {
           line_receive[mon_index++] = aChar;
@@ -74,13 +71,13 @@ void handleCommBridge() {
       }
     }
 
-    // A. TAKOJ pošlji nepoškodovane podatke na WiFi (Prioriteta!)
+    // A. Sending data to WiFi 
     if (pfodClient && inWiFI > 0) {
       pfodClient.write(WIFIbuf, inWiFI);
     }
 
-    // B. Šele ko je serijski prenos končan, v miru obdelaj MQTT
-    // To prepreči, da bi client.publish povzročil "smeti" na Serial2 vhodu
+    // B. Only after the serial transmission is complete, process MQTT in peace
+    // This prevents client.publish from causing "garbage" on the Serial2 input
     if (staReceived) {
       esp32_Mqtt_sta(mqttProcessingBuffer);
       staReceived = false; // Ponastavi zastavico
